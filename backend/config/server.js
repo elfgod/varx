fabaee81ccd61c9c2173cce00863671fbe72db31
elfgod <albertogx@icloud.com>
2021-11-28 21:31:34 -0500

    Creating a Strapi Project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7c07fab76f46f3ce904551744c17ea9d'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dca31edf-1e89-45cf-85f0-de75cff4729a'
+};
